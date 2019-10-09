f42d7ce8f860045d0cfd1e0cf9150d75ba4b9ee7
pac48 <pac48@wildcats.unh.edu>
2019-10-08 21:13:38 -0400

    updated blender_processing.py

--- openrocket/blender_processing.py
+++ openrocket/blender_processing.py
@@ -1,25 +1,64 @@
-import asyncio
+import socket
+import bpy
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5000
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(1)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+conn, addr = s.accept()
+conn.settimeout(1)
 
-async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
+class ModalTimerOperator(bpy.types.Operator):
+    """Operator which runs its self from a timer"""
+    bl_idname = "wm.modal_timer_operator"
+    bl_label = "Modal Timer Operator"
 
-    print(f"Received {message!r} from {addr!r}")
+    _timer = None
 
-    print(f"Send: {message!r}")
-    writer.write(data)
-    await writer.drain()
+    def modal(self, context, event):
+        if event.type in {'RIGHTMOUSE', 'ESC'}:
+            self.cancel(context)
+            return {'CANCELLED'}
 
-    print("Close the connection")
-    writer.close()
+        if event.type == 'TIMER':
+            # change theme color, silly!
+            #color = context.user_preferences.themes[0].view_3d.space.gradients.high_gradient
+            #color.s = 1.0
+            #color.h += 0.01
+            try:
+                data = conn.recv(BUFFER_SIZE)
+                print("received data:", data)
+            except Exception:
+                print("Finish")
+                self.cancel(context)
+                return {'CANCELLED'}
 
-async def main():
-    server = await asyncio.start_server(
-        handle_echo, '127.0.0.1', 5000)
+        return {'PASS_THROUGH'}
 
-    addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
+    def execute(self, context):
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.1, context.window)
+        wm.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
 
-    async with server:
-        await server.serve_forever()
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
+        conn.close()
+
+
+def register():
+    bpy.utils.register_class(ModalTimerOperator)
+
+
+def unregister():
+    bpy.utils.unregister_class(ModalTimerOperator)
+
+
+if __name__ == "__main__":
+    register()
+    # test call
+    bpy.ops.wm.modal_timer_operator()
+    s.close()
